a5d4ba10a148f5ddac2b8160d2bf8f86abf2ade3
Trần Hà Duy Anh <zest.tbz@gmail.com>
2025-04-24 20:13:42 +0700

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,22 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'super-secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    # Import các route & model ở đây (sau khi app khởi tạo)
+    from .routes import app_routes
+    from .models import User, File
+    app.register_blueprint(app_routes)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from flask import Flask
-from app import db
-from app.routes import app as app_routes
+from app import create_app
 
-app = Flask(__name__)
-app.secret_key = "super-secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = create_app()
 
-db.init_app(app)
-
-with app.app_context():
-    from app.models import User, File
-    db.create_all()
-
-app.register_blueprint(app_routes)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
