ac3142aa1247f9afe4bb4c99f2e383cc75c22a37
Trần Hà Duy Anh <zest.tbz@gmail.com>
2025-04-24 20:19:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from app import create_app
-
-app = create_app()
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
