fa33e268746c11e35d2fbea84b7fc28344477693
Trần Hà Duy Anh <zest.tbz@gmail.com>
2025-04-24 20:03:52 +0700

    A

--- app/models.py
+++ app/models.py
@@ -1,14 +1,12 @@
-from main import db
-from datetime import datetime
+from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(200))
 
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255))
-    drive_id = db.Column(db.String(255))
-    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
+    filename = db.Column(db.String(200))
+    drive_id = db.Column(db.String(200))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask import session
+from app import db
 from app.routes import app as app_routes
 
 app = Flask(__name__)
@@ -8,12 +7,13 @@ app.secret_key = "super-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-from app.models import User, File
+with app.app_context():
+    from app.models import User, File
+    db.create_all()
 
 app.register_blueprint(app_routes)
 
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)
